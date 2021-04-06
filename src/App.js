0c584f65ef3650cb4e080c79ae30afda9e739b6e
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2021-04-05 21:35:41 -0300

    Adicionada a navbar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/Header/Header";
+
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
