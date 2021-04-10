b5eb05bb7652b2bf1ba4d32cf27bb3e008754e9f
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2021-04-10 12:47:49 -0300

    Adicionada a funcao de adicionar e remover item do carrinho

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -3,24 +3,37 @@ import { connect } from "react-redux";
 
 import "./CheckoutItem.scss";
 
-export const CheckoutItem = ({
-  cartItem: { name, price, imageUrl, quantity },
-}) => {
+import { clearItem, addItem, removeItem } from "../../redux/cart/cartActions";
+
+export const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+  const { name, price, imageUrl, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
+      </span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        &#10005;
+      </div>
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({});
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItem(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
+});
 
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CheckoutItem);
+export default connect(null, mapDispatchToProps)(CheckoutItem);
--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,5 +1,11 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./cartTypes";
-import { addItemToCart } from "./cartUtils";
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  CLEAR_ITEM,
+  REMOVE_ITEM,
+} from "./cartTypes";
+
+import { addItemToCart, removeItemFromCart } from "./cartUtils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -10,10 +16,25 @@ const cartReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
     case TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+
     case ADD_ITEM:
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, payload),
+      };
+
+    case CLEAR_ITEM:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== payload.id
+        ),
+      };
+
+    case REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, payload),
       };
 
     default:
