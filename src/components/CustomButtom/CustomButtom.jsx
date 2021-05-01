df64dda6ef04232ad6314ec67b59dac424ca15c3
Joseph Gabriel <josegabriel.jgs18@gmail.com>
2021-05-01 13:37:21 -0300

    Adicionado servidor com express e aceitando pagamentos com stripe

--- src/components/CustomButtom/CustomButtom.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { CustomButtomContainer } from "./CustomButtomStyles";
-
-const CustomButtom = ({ children, ...props }) => {
-  return <CustomButtomContainer {...props}>{children}</CustomButtomContainer>;
-};
-
-export default CustomButtom;
